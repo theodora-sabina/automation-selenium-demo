f64124bb8ce5c0ec3ad4f5411ea4c60d2cb26c91
Theodora Opinca <theodora.opinca@netcentric.biz>
2021-09-23 14:24:09 +0300

    Updated testng test

--- src/test/java/tests/TestNgTest.java
+++ src/test/java/tests/TestNgTest.java
@@ -2,21 +2,33 @@ package tests;
 
 import java.lang.reflect.Method;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNgTest extends TestBase{
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("This is before class");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("This is before method");
+    }
 
     @Test(description = "test 1", dependsOnGroups = "Smoke test")
     public void test1(){
         System.out.println("This is the first test");
     }
 
-    @Test(description = "test 2", enabled = false)
+    @Test(description = "test 2")
     public void test2(Method testMethod){
-        System.out.println("This is " );
+        System.out.println("This is second test" );
     }
 
-    @Test( timeOut = 500, alwaysRun = true)
+    @Test( timeOut = 5000, alwaysRun = true)
     public void test3() throws InterruptedException {
         Thread.sleep(2000);
         System.out.println("This is the third test");
